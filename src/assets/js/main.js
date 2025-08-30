d16a8dfa759dc905e6e28b6ffe187bf697388c78
ZANX3Y <74610540+ZANX3Y@users.noreply.github.com>
2025-08-30 17:52:19 +0200

    Add copy fallback for older browsers

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -98,25 +98,46 @@ const pgpKey = document.getElementById('pgp-key')
 const copyPgpBtn = document.getElementById('copy-pgp')
 const copyPgpBtnIcon = copyPgpBtn.querySelector('i')
 const copyPgpBtnText = copyPgpBtn.querySelector('span')
+let copyTimeout = undefined
 
-function resetCopyButton() {
-    setTimeout(() => {
-        copyPgpBtnIcon.textContent = 'content_copy'
-        copyPgpBtnText.textContent = 'Copy Key'
-    }, 2000)
-}
+const copyFallback = (text) => new Promise((resolve, reject) => {
+    const textArea = document.createElement('textarea')
+    textArea.value = text
+    textArea.style.position = 'absolute'
+    textArea.style.left = '-9999px'
+    document.body.appendChild(textArea)
+    textArea.select()
+
+    try {
+        document.execCommand('copy') ? resolve() : reject()
+    } catch (err) {
+        reject()
+    } finally {
+        document.body.removeChild(textArea)
+    }
+})
 
 copyPgpBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText(pgpKey.textContent.trim())
+    const text = pgpKey.textContent.trim()
+
+    const promise = (navigator.clipboard && window.isSecureContext)
+        ? navigator.clipboard.writeText(text)
+        : copyFallback(text)
+
+    promise
         .then(() => {
             copyPgpBtnIcon.textContent = 'check'
             copyPgpBtnText.textContent = 'Copied!'
-            resetCopyButton()
         })
-        .catch(err => {
-            console.error('Failed to copy: ', err)
+        .catch(() => {
             copyPgpBtnIcon.textContent = 'close'
             copyPgpBtnText.textContent = 'Error'
-            resetCopyButton()
+        })
+        .finally(() => {
+            clearTimeout(copyTimeout)
+            copyTimeout = setTimeout(() => {
+                copyPgpBtnIcon.textContent = 'content_copy'
+                copyPgpBtnText.textContent = 'Copy Key'
+            }, 2000)
         })
 })
